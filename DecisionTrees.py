061cd78f9f32b53b8e6b9ebaa6d927019aa1aad3
Revanth Pentyala <revanthp@users.noreply.github.com>
2020-10-27 00:52:21 -0700

    Create DecisionTrees.py

---
+++ DecisionTrees.py
@@ -0,0 +1,25 @@
+# implementing a decision trees
+# input 
+#   parameters:
+#   - tree_depth
+#   - min_sample_leaf
+#   - min_sample_split
+# output :
+#   - tree model
+
+def calculate_entropy(arr, bkpt):
+  pass
+  
+def calculate_gini(arr, bkpt):
+  pass
+
+Class DecisionTreeClassifier:
+  def __init__(self, max_depth, min_sample_leaf, min_sample_split, random_state):
+    pass
+    
+  def train(self, X, y):
+    pass
+    
+  def predict(self, X):
+    pass
+ 
